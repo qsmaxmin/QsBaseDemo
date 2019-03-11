ac4e02e4721fc025149be75cffdfbb2f39b23de6
qsmaxmin <17600662013@163.com>
2019-03-11 10:01:07 +0800

    demo框架更新

--- app/src/main/java/com/sugar/grapecollege/common/http/CustomHttpCallback.java
+++ app/src/main/java/com/sugar/grapecollege/common/http/CustomHttpCallback.java
@@ -3,6 +3,7 @@ package com.sugar.grapecollege.common.http;
 import com.qsmaxmin.qsbase.common.http.HttpBuilder;
 import com.qsmaxmin.qsbase.common.http.HttpResponse;
 import com.qsmaxmin.qsbase.common.http.QsHttpCallback;
+import com.qsmaxmin.qsbase.common.log.L;
 import com.sugar.grapecollege.common.model.AppConstants;
 
 /**
@@ -14,6 +15,7 @@ public class CustomHttpCallback implements QsHttpCallback {
 
     @Override public void initHttpAdapter(HttpBuilder builder) throws Exception {
         //custom logic
+        L.i("CustomHttpCallback", "initHttpAdapter....path:" + builder.getPath());
         builder.setTerminal("http://www.baidu.com");
         builder.addHeader("Content-Type", "application/json");
         builder.addHeader("os", AppConstants.APP_OS);
@@ -22,9 +24,11 @@ public class CustomHttpCallback implements QsHttpCallback {
 
     @Override public void onHttpResponse(HttpResponse response) throws Exception {
         //custom logic
+        L.i("CustomHttpCallback", "onHttpResponse....path:" + response.getHttpBuilder().getPath());
     }
 
     @Override public void onResult(HttpBuilder builder, Object result) throws Exception {
         //custom logic
+        L.i("CustomHttpCallback", "onResult....path:" + builder.getPath());
     }
 }
