21e98b3e64283964c04d5a1de13435b04159c780
qsmaxmin <17600662013@163.com>
2019-06-21 16:03:38 +0800

    demo框架更新

--- app/src/main/java/com/sugar/grapecollege/common/dialog/CustomDialog.java
+++ app/src/main/java/com/sugar/grapecollege/common/dialog/CustomDialog.java
@@ -1,10 +1,8 @@
 package com.sugar.grapecollege.common.dialog;
 
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
+import com.qsmaxmin.qsbase.common.viewbind.annotation.OnClick;
 import com.qsmaxmin.qsbase.common.widget.dialog.QsDialogFragment;
 import com.sugar.grapecollege.R;
 
@@ -14,32 +12,22 @@ import com.sugar.grapecollege.R;
  * @Description
  */
 
-public class CustomDialog extends QsDialogFragment implements View.OnClickListener {
+public class CustomDialog extends QsDialogFragment {
 
     @Override protected int getDialogTheme() {
         return R.style.BottomTopDialogStyle;
     }
 
-    @Override protected View getDialogView(LayoutInflater layoutInflater, ViewGroup viewGroup) {
-        View view = layoutInflater.inflate(R.layout.dialog_compatible, viewGroup);
-        setView(view);
-        return view;
+    @Override protected int layoutId() {
+        return R.layout.dialog_compatible;
     }
 
-    private void setView(View view) {
-        View view_click = view.findViewById(R.id.view_click);
-        TextView tv_positive = (TextView) view.findViewById(R.id.tv_positive);
-        TextView tv_neutral = (TextView) view.findViewById(R.id.tv_neutral);
-        TextView tv_negative = (TextView) view.findViewById(R.id.tv_negative);
-
-        view_click.setOnClickListener(this);
-        tv_positive.setOnClickListener(this);
-        tv_neutral.setOnClickListener(this);
-        tv_negative.setOnClickListener(this);
+    @Override protected void initData() {
     }
 
-    @Override public void onClick(View v) {
-        switch (v.getId()) {
+    @OnClick({R.id.view_click, R.id.tv_positive, R.id.tv_neutral, R.id.tv_negative})
+    @Override public void onViewClick(View view) {
+        switch (view.getId()) {
             case R.id.view_click:
                 break;
             case R.id.tv_positive:
@@ -51,4 +39,5 @@ public class CustomDialog extends QsDialogFragment implements View.OnClickListen
         }
         dismissAllowingStateLoss();
     }
+
 }
