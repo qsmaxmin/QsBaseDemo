246d3757fe0e8b99c50b1859226fe3270f278531
qsmaxmin <871245760@qq.com>
2020-10-30 16:27:51 +0800

    框架更新

--- app/src/main/java/com/sugar/grapecollege/common/aspect/LoginAspect.java
+++ app/src/main/java/com/sugar/grapecollege/common/aspect/LoginAspect.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 
 import com.qsmaxmin.annotation.aspect.JoinPoint;
 import com.qsmaxmin.annotation.aspect.QsIAspect;
+import com.qsmaxmin.qsbase.common.log.L;
 import com.qsmaxmin.qsbase.common.utils.QsHelper;
 import com.sugar.grapecollege.common.model.UserConfig;
 import com.sugar.grapecollege.user.LoginActivity;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -19,8 +21,14 @@ public class LoginAspect implements QsIAspect {
     private static HashMap<Integer, JoinPoint> holder;
 
     @Override public Object around(JoinPoint point) {
+        L.i("LoginAspect", "around........target: " + point.getTarget());
+        L.i("LoginAspect", "around........args: " + Arrays.toString(point.getArgs()));
+        L.i("LoginAspect", "around........tag: " + point.getTag());
+
         if (UserConfig.getInstance().isLogin()) {
-            return point.proceed();
+            Object proceed = point.proceed();
+            L.i("LoginAspect", "around(login)........execute original method complete, return value: " + proceed);
+            return proceed;
         } else {
             if (holder == null) {
                 holder = new HashMap<>();
@@ -28,6 +36,7 @@ public class LoginAspect implements QsIAspect {
             int requestCode = 996;
             holder.put(requestCode, point);
 
+            L.i("LoginAspect", "around(no login)........go LoginActivity");
             Bundle bundle = new Bundle();
             bundle.putInt(LoginActivity.BK_REQUEST_CODE, requestCode);
             QsHelper.intent2Activity(LoginActivity.class, bundle);
@@ -36,9 +45,12 @@ public class LoginAspect implements QsIAspect {
     }
 
     public static void onLogin(int requestCode) {
-        JoinPoint joinPoint = holder.get(requestCode);
-        if (joinPoint != null) {
-            joinPoint.proceed();
+        if (holder != null) {
+            JoinPoint joinPoint = holder.remove(requestCode);
+            if (joinPoint != null) {
+                joinPoint.proceed();
+                L.i("LoginAspect", "around(no login)........execute original method complete, loss return value !!!!");
+            }
         }
     }
 }
