6195492147acf9c958dea13a684d086c020df7e9
qsmaxmin <871245760@qq.com>
2021-02-02 11:36:32 +0800

    框架更新,demo完善

--- app/src/main/java/com/sugar/grapecollege/test/fragment/TestFragment.java
+++ app/src/main/java/com/sugar/grapecollege/test/fragment/TestFragment.java
@@ -1,12 +1,18 @@
 package com.sugar.grapecollege.test.fragment;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.qsmaxmin.annotation.thread.ThreadPoint;
+import com.qsmaxmin.annotation.thread.ThreadType;
 import com.sugar.grapecollege.R;
 import com.sugar.grapecollege.common.base.fragment.BaseFragment;
+import com.sugar.grapecollege.common.http.HomeHttp;
+import com.sugar.grapecollege.common.http.resp.ModelHomeHeader;
+import com.sugar.grapecollege.common.model.UserConfig;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -28,6 +34,18 @@ public class TestFragment extends BaseFragment {
     }
 
     @Override public void initData(Bundle bundle) {
+        requestData();
+    }
+
+    @ThreadPoint(ThreadType.HTTP)
+    private void requestData() {
+        SystemClock.sleep(1000);
+        HomeHttp http = createHttpRequest(HomeHttp.class);
+        ModelHomeHeader resp = http.requestHomeHeaderData(UserConfig.getInstance().userId);
+        if (resp != null) {
+            //todo custom logic......
+        }
         showContentView();
+//        showErrorView();
     }
 }
